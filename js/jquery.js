c3f8d478b2c62c17e42ea8866f5569d21ef5913a
Melissa Knapp <knappm124@gmail.com>
2022-12-27 20:39:24 -0500

    fixed jquery file and confirmed working

--- js/jquery.js
+++ js/jquery.js
@@ -2,25 +2,13 @@ jQuery(()=> {
     let month = document.getElementById('month');
     let day = document.getElementById('day');
     month.addEventListener('change', () => {
-        switch ($('#month').val) {
-            case 'february':
-                day.attr('max','28');
-                break;
-            case 'april':
-                day.attr('max', '30');
-                break;
-            case 'june':
-                day.attr('max', '30');
-                break;
-            case 'september':
-                day.attr('max', '30');
-                break;
-            case 'november':
-                day.attr('max', '30');
-                break;
-            default:
-                day.attr('max', '31');
-                break;
+        if(month.value == 'february'){
+            alert('month has changed');
+            day.setAttribute('max','28');           
+        } else if(month.value == 'april' || month.value == 'june' || month.value == 'september' || month.value == 'november'){
+            day.setAttribute('max', '30');
+        }else {
+            day.setAttribute('max', '31');
         }
     })
 })
